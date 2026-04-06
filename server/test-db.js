c64ba5a83d9d1a8a6d4900ad17c8c303feced88f
Sarfaraz Ahmad <babusarfarazahmad777@gmail.com>
2026-04-06 20:01:13 +0530

    Remove hardcoded MongoDB URI from test script

--- server/test-db.js
+++ server/test-db.js
@@ -1,15 +1,29 @@
+const path = require('path');
+const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+dotenv.config({ path: path.join(__dirname, '.env') });
+
 async function main() {
-  await mongoose.connect('mongodb+srv://sarfaraz_db_user:mD93VARAJHUVpNkX@cluster0.nkgdh6e.mongodb.net/?appName=Cluster0');
-  
+  const mongoUri = process.env.MONGO_URI;
+
+  if (!mongoUri) {
+    throw new Error('MONGO_URI is not defined. Add it to server/.env before running this script.');
+  }
+
+  await mongoose.connect(mongoUri);
+
   try {
     await mongoose.connection.collection('users').dropIndex('username_1');
     console.log('Dropped username_1 index');
   } catch (err) {
     console.log('Error dropping index', err);
+  } finally {
+    await mongoose.connection.close();
   }
-  
-  process.exit(0);
 }
-main().catch(console.error);
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
